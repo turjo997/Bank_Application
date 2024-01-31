536ffae2837a65d1b4e310a2e43b166a44d99cb9
turjo997 <turja997@gmail.com>
2024-01-31 21:02:45 +0600

    All the necessary methods is declared without implementation

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Account{
 
     private String name;
@@ -70,7 +73,97 @@ class Account{
 
 public class Main {
 
+    private ArrayList<Account> accounts;
+    private Scanner scanner;
+
+    public Main(){
+        accounts = new ArrayList<>();
+        scanner = new Scanner(System.in);
+    }
+
+    public void startApp(){
+
+        int choice;
+        do {
+            System.out.println("1. Create a new account");
+            System.out.println("2. Display all accounts");
+            System.out.println("3. Update an account");
+            System.out.println("4. Delete an account");
+            System.out.println("5. Deposit an amount into your account");
+            System.out.println("6. Withdraw an amount from your account");
+            System.out.println("7. Search for account");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    createAccount();
+                    break;
+                case 2:
+                    displayAllAccounts();
+                    break;
+                case 3:
+                    updateAccount();
+                    break;
+                case 4:
+                    deleteAccount();
+                    break;
+                case 5:
+                    depositAmount();
+                    break;
+                case 6:
+                    withdrawAmount();
+                    break;
+                case 7:
+                    searchAccount();
+                    break;
+                case 8:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 8.");
+            }
+        } while (choice != 8);
+
+
+
+
+
+    }
+
+    private void deleteAccount() {
+
+    }
+
+    private void depositAmount() {
+
+    }
+
+    private void withdrawAmount() {
+
+    }
+
+    private void searchAccount() {
+
+    }
+
+    private void updateAccount() {
+
+    }
+
+    private void displayAllAccounts() {
+
+    }
+
+    private void createAccount() {
+
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main app = new Main();
+
+        app.startApp();
+
     }
 }
